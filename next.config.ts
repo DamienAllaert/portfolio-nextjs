f9d02f06a910bfd74b08494bc7ae2ec3829074fd
Damien ALLAERT <damien.allaert.59@gmail.com>
2025-01-16 22:03:19 +0100

    Correction pour les fichier static

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     // Add any other configuration options here
     pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
