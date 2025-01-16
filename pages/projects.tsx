2c7157e5a269757c6a0cbd13255c15cf6b219fd2
DamienAllaert <133202768+DamienAllaert@users.noreply.github.com>
2025-01-17 00:35:39 +0100

    Update projects.tsx
    Ajout d'un slash pour le path de l'image du projet

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
             <div className="min-h-screen flex flex-col items-center justify-center ">
                 <h1 className="text-3xl font-bold text-gray-800">Mes Projets</h1>
                 <div className="flex space-x-5 mt-8">
-                    <ProjectCardProps title={"Marathon du Web"} imageUrl={"portfolio-nextjs/images/marathonWeb.png"} githubUrl={"https://github.com/DamienAllaert/MarathonWeb"} description={"Un évènement durant lequel les étudiants créer un site pendant 48h"}/>
+                    <ProjectCardProps title={"Marathon du Web"} imageUrl={"/portfolio-nextjs/images/marathonWeb.png"} githubUrl={"https://github.com/DamienAllaert/MarathonWeb"} description={"Un évènement durant lequel les étudiants créer un site pendant 48h"}/>
                 </div>
             </div>
         </div>
