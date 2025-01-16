21cc46754d86aeb8a0c0ec3f67f7cc09dbf3e71e
Damien ALLAERT <damien.allaert.59@gmail.com>
2025-01-16 22:25:49 +0100

    Ajout d'un basePath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/portfolio-nextjs',
     reactStrictMode: true,
     // Add any other configuration options here
     pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
